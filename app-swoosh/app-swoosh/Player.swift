da070ea25212f94d30cc7054265fcba293eb708d
Pulkit Gulati <pulkit.gemini@gmail.com>
2018-05-03 00:28:12 +0530

    passing data with Model.

---
+++ app-swoosh/app-swoosh/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Pulkit Gulati on 03/05/18.
+//  Copyright © 2018 Pulkit Gulati LLC. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
